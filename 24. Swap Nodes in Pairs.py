9bc631e471f097371c496f0d6f96777f74d90008
Anubhab Swain <62087261+anubhab-code@users.noreply.github.com>
2020-05-16 19:42:31 +0530

    Create 24. Swap Nodes in Pairs.py

---
+++ 24. Swap Nodes in Pairs.py
@@ -0,0 +1,12 @@
+class Solution:
+    def swapPairs(self, head):
+        if head is None:
+            return head
+        
+        ref = head
+        
+        while ref is not None and ref.next is not None:
+            ref.val, ref.next.val = ref.next.val, ref.val
+            ref = ref.next.next
+            
+        return head
